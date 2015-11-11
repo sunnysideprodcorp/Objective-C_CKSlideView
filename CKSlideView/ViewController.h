0c6f5ba16f7b924e5fe9fd0b9ee0469f095c11c3
sunnysideprodcorp <sunnysideprodcorp@gmail.com>
2015-11-10 22:18:47 -0500

    Initial Commit

---
+++ CKSlideView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CKSlideView
+//
+//  Created by Aileen Nielsen on 11/10/15.
+//  Copyright © 2015 SunnysideProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
